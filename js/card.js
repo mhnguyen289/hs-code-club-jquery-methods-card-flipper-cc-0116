f466d70034f94951967bd658e5a95f3b9a018783
mhnguyen289 <mhnguyen289@gmail.com>
2016-04-19 15:12:58 -0700

    Done.

--- js/card.js
+++ js/card.js
@@ -1,11 +1,16 @@
 $( function(){
 
   $( "#card-one").click(function(){
-    //Use toggleClass method here
+    $(this).toggleClass('flipped')
   });
 
-
-	// code click event for card-two here
+  // code click event for card-two here
+  $( "#card-two").click(function(){
+    $(this).toggleClass('flipped')
+  });
 
   // code click event for card-three here
+   $( "#card-three").click(function(){
+    $(this).toggleClass('flipped')
+  });
  })
